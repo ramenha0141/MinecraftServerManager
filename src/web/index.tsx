f5aad91e62cd48d679ef23cf78f2246d243b07f6
ramenha0141 <ramenhaoishii@gmail.com>
2022-06-30 15:59:48 +0900

    update

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 
 import { App } from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RecoilRoot>
+            <App />
+        </RecoilRoot>
     </StrictMode>
 );
