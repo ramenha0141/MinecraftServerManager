46453332a5393a1e63276cea4c0190175de20d54
ramenha0141 <ramenhaoishii@gmail.com>
2022-07-11 21:33:37 +0900

    console auto scroll

--- src/console/index.ts
+++ src/console/index.ts
@@ -1,4 +1,16 @@
 const ConsoleAPI = window.ConsoleAPI;
 
+let autoScroll = true;
+
 const root = document.getElementById('root')!;
-ConsoleAPI.onData((_, data: string) => root.appendChild(document.createTextNode(data)));
+root.addEventListener('scroll', () => {
+    if (root.scrollHeight - (root.clientHeight + root.scrollTop) === 0) {
+        autoScroll = true;
+    } else {
+        autoScroll = false;
+    }
+});
+ConsoleAPI.onData((_, data: string) => {
+    root.appendChild(document.createTextNode(data));
+    if (autoScroll) root.scroll(0, root.scrollHeight);
+});
