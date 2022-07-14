ee92bfff45571116c0867e0b65f2595e9fbd96d4
ramenha0141 <ramenhaoishii@gmail.com>
2022-07-14 16:08:11 +0900

    update

--- src/app/Control.tsx
+++ src/app/Control.tsx
@@ -2,7 +2,7 @@ import { Alert, Button, ButtonGroup, Snackbar } from '@mui/material';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import StopIcon from '@mui/icons-material/Stop';
 import TerminalIcon from '@mui/icons-material/Terminal';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 const ServerAPI = window.ServerAPI;
 
@@ -13,66 +13,68 @@ const Control = () => {
     const [isProcessing, setIsProcessing] = useState<boolean>(false);
     const [isShowAlert, setIsShowAlert] = useState<boolean>(false);
     const [alertType, setAlertType] = useState<alertType>();
-    const handleStart = async () => {
-        if (isServerRunning) return;
-        setIsProcessing(true);
-        if (await ServerAPI.start()) {
-            setIsServerRunning(true);
-            showAlert('startSuccess');
-        } else {
-            showAlert('startFailed');
-        }
-        setIsProcessing(false);
-    };
-    const handleStop = async () => {
-        if (!isServerRunning) return;
-        setIsProcessing(true);
-        if (await ServerAPI.stop()) {
-            setIsServerRunning(false);
-            showAlert('stopSuccess');
-        }
-        setIsProcessing(false);
-    };
-    const handleShowConsole = () => {
-        ServerAPI.showConsole();
-    };
-    const showAlert = (type: alertType) => {
-        setAlertType(type);
-        setIsShowAlert(true);
-    };
-    const handleAlertClose = () => {
-        setIsShowAlert(false);
-    };
-    return (
-        <>
-            <ButtonGroup variant='contained' sx={{ mr: 10, backgroundColor: '#fff' }}>
-                <Button
-                    variant='outlined'
-                    disabled={isServerRunning || isProcessing}
-                    onClick={handleStart}
-                ><PlayArrowIcon /></Button>
-                <Button
-                    variant='outlined'
-                    disabled={!isServerRunning || isProcessing}
-                    onClick={handleStop}
-                ><StopIcon /></Button>
-                <Button
-                    variant='outlined'
-                    onClick={handleShowConsole}
-                ><TerminalIcon /></Button>
-            </ButtonGroup>
-            <Snackbar open={isShowAlert} autoHideDuration={5000} anchorOrigin={{ vertical: 'top', horizontal: 'center' }} onClose={handleAlertClose}>
-                <Alert severity={alertType === 'startFailed' ? 'error' : 'success'} sx={{ width: '100%'}} onClose={handleAlertClose}>
-                    {
-                        alertType === 'startSuccess'
-                            ? 'サーバーが正常に起動しました'
-                            : alertType === 'startFailed'
-                            ? 'サーバーの起動に失敗しました'
-                            : 'サーバーが正常に停止しました'
-                    }
-                </Alert>
-            </Snackbar>
-        </>
-    );
+    return useMemo(() => {
+        const handleStart = async () => {
+            if (isServerRunning) return;
+            setIsProcessing(true);
+            if (await ServerAPI.start()) {
+                setIsServerRunning(true);
+                showAlert('startSuccess');
+            } else {
+                showAlert('startFailed');
+            }
+            setIsProcessing(false);
+        };
+        const handleStop = async () => {
+            if (!isServerRunning) return;
+            setIsProcessing(true);
+            if (await ServerAPI.stop()) {
+                setIsServerRunning(false);
+                showAlert('stopSuccess');
+            }
+            setIsProcessing(false);
+        };
+        const handleShowConsole = () => {
+            ServerAPI.showConsole();
+        };
+        const showAlert = (type: alertType) => {
+            setAlertType(type);
+            setIsShowAlert(true);
+        };
+        const handleAlertClose = () => {
+            setIsShowAlert(false);
+        };
+        return (
+            <>
+                <ButtonGroup variant='contained' sx={{ mr: 10, backgroundColor: '#fff' }}>
+                    <Button
+                        variant='outlined'
+                        disabled={isServerRunning || isProcessing}
+                        onClick={handleStart}
+                    ><PlayArrowIcon /></Button>
+                    <Button
+                        variant='outlined'
+                        disabled={!isServerRunning || isProcessing}
+                        onClick={handleStop}
+                    ><StopIcon /></Button>
+                    <Button
+                        variant='outlined'
+                        onClick={handleShowConsole}
+                    ><TerminalIcon /></Button>
+                </ButtonGroup>
+                <Snackbar open={isShowAlert} autoHideDuration={5000} anchorOrigin={{ vertical: 'top', horizontal: 'center' }} onClose={handleAlertClose}>
+                    <Alert severity={alertType === 'startFailed' ? 'error' : 'success'} sx={{ width: '100%' }} onClose={handleAlertClose}>
+                        {
+                            alertType === 'startSuccess'
+                                ? 'サーバーが正常に起動しました'
+                                : alertType === 'startFailed'
+                                    ? 'サーバーの起動に失敗しました'
+                                    : 'サーバーが正常に停止しました'
+                        }
+                    </Alert>
+                </Snackbar>
+            </>
+        );
+    }, [isServerRunning, isProcessing, isShowAlert, alertType]);
 };
 export default Control;
--- src/app/Setup.tsx
+++ src/app/Setup.tsx
@@ -10,115 +10,116 @@ const Setup = (props: { setIsInstalled: (value: boolean) => void }) => {
     const [currentStep, setCurrentStep] = useState<number>(0);
     const [isProcessing, setIsProcessing] = useState<boolean>(false);
     const [version, setVersion] = useState<versions>('1.19');
-    const handleVersionChange = (event: SelectChangeEvent) => {
-        setVersion(event.target.value as versions);
-    };
-    const handleInstall = async () => {
-        setIsProcessing(true);
-        const isInstallSuccess = await ServerAPI.install(version);
-        setIsProcessing(false);
-        if (isInstallSuccess) {
-            setCurrentStep(1);
-        }
-    };
-    const handleAgree = async () => {
-        setIsProcessing(true);
-        const isAgreeSuccess = await ServerAPI.agreeEULA();
-        setIsProcessing(false);
-        if (isAgreeSuccess) {
-            setCurrentStep(2);
-        }
-    };
-    const handleFinish = async () => {
-        setCurrentStep(3);
-        props.setIsInstalled(true);
-    };
-    return useMemo(() => (
-        <Box sx={{
-            flexGrow: 1,
-            px: 4,
-            py: 4,
-            display: 'flex',
-            flexDirection: 'column'
-        }}>
+    return useMemo(() => {
+        const handleVersionChange = (event: SelectChangeEvent) => {
+            setVersion(event.target.value as versions);
+        };
+        const handleInstall = async () => {
+            setIsProcessing(true);
+            const isInstallSuccess = await ServerAPI.install(version);
+            setIsProcessing(false);
+            if (isInstallSuccess) {
+                setCurrentStep(1);
+            }
+        };
+        const handleAgree = async () => {
+            setIsProcessing(true);
+            const isAgreeSuccess = await ServerAPI.agreeEULA();
+            setIsProcessing(false);
+            if (isAgreeSuccess) {
+                setCurrentStep(2);
+            }
+        };
+        const handleFinish = async () => {
+            setCurrentStep(3);
+            props.setIsInstalled(true);
+        };
+        return (
             <Box sx={{
                 flexGrow: 1,
-                pb: 1,
+                px: 4,
+                py: 4,
                 display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
+                flexDirection: 'column'
             }}>
-                {
-                    currentStep === 0
-                        ? (
-                            <>
-                                <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-                                    MinecraftServerManager へようこそ！
-                                </Typography>
-                                <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-                                    Minecraft Server をインストールする必要があります。
-                                    <br></br>
-                                    バージョンを選択し、インストールを押してインストールを開始してください。
-                                </Typography>
-                                <Select
-                                    label='バージョン'
-                                    value={version}
-                                    onChange={handleVersionChange}
-                                >
-                                    <MenuItem value='1.19'>1.19</MenuItem>
-                                    <MenuItem value='1.18.2'>1.18.2</MenuItem>
-                                    <MenuItem value='1.16.5'>1.16.5</MenuItem>
-                                    <MenuItem value='1.12.2'>1.12.2</MenuItem>
-                                </Select>
-                                <Button
-                                    variant='contained'
-                                    size='large'
-                                    onClick={handleInstall}
-                                    sx={{ my: 10 }}
-                                >インストール</Button>
-                            </>
-                        ) : currentStep === 1
+                <Box sx={{
+                    flexGrow: 1,
+                    pb: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                }}>
+                    {
+                        currentStep === 0
                             ? (
                                 <>
-                                    <webview
-                                        src='https://aka.ms/MinecraftEULA'
-                                        style={{ flexGrow: 1, width: '80%' }}
-                                    />
-                                    <Button
-                                        variant='contained'
-                                        sx={{ mt: 1 }}
-                                        onClick={handleAgree}
-                                    >同意</Button>
-                                </>
-                            ) : (
-                                <>
+                                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                                        MinecraftServerManager へようこそ！
+                                    </Typography>
+                                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                                        Minecraft Server をインストールする必要があります。
+                                        <br></br>
+                                        バージョンを選択し、インストールを押してインストールを開始してください。
+                                    </Typography>
+                                    <Select
+                                        value={version}
+                                        onChange={handleVersionChange}
+                                    >
+                                        <MenuItem value='1.19'>1.19</MenuItem>
+                                        <MenuItem value='1.18.2'>1.18.2</MenuItem>
+                                        <MenuItem value='1.16.5'>1.16.5</MenuItem>
+                                        <MenuItem value='1.12.2'>1.12.2</MenuItem>
+                                    </Select>
                                     <Button
                                         variant='contained'
                                         size='large'
-                                        onClick={handleFinish}
-                                    >完了</Button>
+                                        onClick={handleInstall}
+                                        sx={{ my: 10 }}
+                                    >インストール</Button>
                                 </>
+                            ) : currentStep === 1
+                                ? (
+                                    <>
+                                        <webview
+                                            src='https://aka.ms/MinecraftEULA'
+                                            style={{ flexGrow: 1, width: '80%' }}
+                                        />
+                                        <Button
+                                            variant='contained'
+                                            sx={{ mt: 1 }}
+                                            onClick={handleAgree}
+                                        >同意</Button>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Button
+                                            variant='contained'
+                                            size='large'
+                                            onClick={handleFinish}
+                                        >完了</Button>
+                                    </>
+                                )
+                    }
+                </Box>
+                <Stepper activeStep={currentStep}>
+                    {
+                        steps.map((step, i) => {
+                            return (
+                                <Step key={step} completed={i < currentStep}>
+                                    <StepLabel>{step}</StepLabel>
+                                </Step>
                             )
-                }
+                        })
+                    }
+                </Stepper>
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={isProcessing}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
             </Box>
-            <Stepper activeStep={currentStep}>
-                {
-                    steps.map((step, i) => {
-                        return (
-                            <Step key={step} completed={i < currentStep}>
-                                <StepLabel>{step}</StepLabel>
-                            </Step>
-                        )
-                    })
-                }
-            </Stepper>
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={isProcessing}
-            >
-                <CircularProgress color="inherit" />
-            </Backdrop>
-        </Box>
-    ), [currentStep, isProcessing, version]);
+        );
+    }, [currentStep, isProcessing, version]);
 };
 export default Setup;
